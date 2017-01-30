49fef96a7260f9cd92433f85e61d0bb085b2ca18
Marcelo <marceloaldo@gmail.com>
2017-01-30 19:01:47 -0300

    Protecting the routes

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,24 @@
 |
 */
 
-$app->get('users', 'UsersController@index');
-$app->get('users/{user}', 'UsersController@show');
-$app->post('users', 'UsersController@store');
-$app->patch('users/{user}', 'UsersController@update');
-$app->delete('users/{user}', 'UsersController@destroy');
+$app->group(['middleware' => 'auth:api'], function ($app) {
+    $app->get('users', 'UsersController@index');
+    $app->get('users/{user}', 'UsersController@show');
+    $app->post('users', 'UsersController@store');
+    $app->patch('users/{user}', 'UsersController@update');
+    $app->delete('users/{user}', 'UsersController@destroy');
 
-$app->get('priorities', 'PrioritiesController@index');
-$app->get('priorities/{priority}', 'PrioritiesController@show');
-$app->post('priorities', 'PrioritiesController@store');
-$app->patch('priorities/{priority}', 'PrioritiesController@update');
-$app->delete('priorities/{priority}', 'PrioritiesController@destroy');
+    $app->get('priorities', 'PrioritiesController@index');
+    $app->get('priorities/{priority}', 'PrioritiesController@show');
+    $app->post('priorities', 'PrioritiesController@store');
+    $app->patch('priorities/{priority}', 'PrioritiesController@update');
+    $app->delete('priorities/{priority}', 'PrioritiesController@destroy');
 
-$app->get('tasks', 'TasksController@index');
-$app->get('tasks/{task}', 'TasksController@show');
-$app->post('tasks', 'TasksController@store');
-$app->patch('tasks/{task}', 'TasksController@update');
-$app->delete('tasks/{task}', 'TasksController@destroy');
+    $app->get('tasks', 'TasksController@index');
+    $app->get('tasks/{task}', 'TasksController@show');
+    $app->post('tasks', 'TasksController@store');
+    $app->patch('tasks/{task}', 'TasksController@update');
+    $app->delete('tasks/{task}', 'TasksController@destroy');
+});
 
 $app->post('auth/login', 'AuthController@login');
